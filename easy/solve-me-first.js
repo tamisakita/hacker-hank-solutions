a589a4310a9ccf555d1bebeca29979fee669b45d
Tami Pipoca <tamisakita@hotmail.com>
2021-04-27 21:48:40 -0300

    add solutions

---
+++ easy/solve-me-first.js
@@ -0,0 +1,22 @@
+// Complete the function solveMeFirst to compute the sum of two integers.
+
+// Example
+//a=7
+//b=3
+
+// Return 10.
+
+// Function Description
+
+// Complete the solveMeFirst function in the editor below.
+
+// solveMeFirst has the following parameters:
+
+// int a: the first value
+// int b: the second value
+// Returns
+// - int: the sum of  and 
+
+function solveMeFirst(a, b) {
+  return a + b;
+}
